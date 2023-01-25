c459f357d6b98647b87854b472f0d9852be714fa
Jan <janrubio@utexas.edu>
2023-01-24 21:53:11 -0600

    added regex inline test 1

--- jan_inline_test_1.py
+++ jan_inline_test_1.py
@@ -1,8 +1,24 @@
+import sys
 import re
+from inline import Here
 
+input_list = sys.argv[1:]
+
+# String to "tokenize"
 txt = "Have you anything ever had a dream that you, um, you had, your, you- you could, " \
-        "you’ll do, you- you wants, you, you could do so, you- you’ll do, you could- " \
-        "you, you want, you want him to do you so much you could do anything?"
+    "you’ll do, you- you wants, you, you could do so, you- you’ll do, you could- " \
+    "you, you want, you want him to do you so much you could do anything?"
 
-x = re.findall(".*,", txt)
-print(x)
+# parse string
+res = re.findall(r"[- ’A-Za-z]*,|[- ’A-Za-z]*\?", txt)
+idx = 0
+for tok in res:
+	tok = tok.replace(",", "")
+	res[idx] = tok
+	idx += 1
+
+# answer
+ans = txt.split(",")
+
+# verify
+Here().given(input_list, txt).check_eq(res, ans)
